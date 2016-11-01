a401c924e5fe3cba4d2fd72e1c9bd1054c9f91ea
Geoff Harcourt <geoff.harcourt@gmail.com>
2016-10-31 22:01:21 -0400

    Load fzf in zsh config

--- zsh/configs/post/fzf.zsh
+++ zsh/configs/post/fzf.zsh
@@ -1,8 +1,8 @@
 export FZF_CTRL_T_COMMAND='ag -g ""'
 export FZF_DEFAULT_COMMAND='ag -g ""'
-export FZF_DEFAULT_OPTS='
-  --bind ctrl-f:page-down,ctrl-b:page-up
-'
+export FZF_DEFAULT_OPTS=' --bind ctrl-f:page-down,ctrl-b:page-up '
 
 # This should be rewritten to just use the fzf installed by homebrew
 [ -f /usr/local/opt/fzf/shell/key-bindings.zsh ] && source /usr/local/opt/fzf/shell/key-bindings.zsh
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
