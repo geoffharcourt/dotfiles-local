9ff95ff281acd140695dff2f5449261a0c0e39f0
Geoff Harcourt <geoff.harcourt@gmail.com>
2018-06-23 22:51:03 -0400

    Use Heroku's default autocomplete, correctly this time

--- zsh/configs/heroku.zsh
+++ zsh/configs/heroku.zsh
@@ -1,1 +1,1 @@
-heroku autocomplete:script zsh
+HEROKU_AC_ZSH_SETUP_PATH=/Users/geoff/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
