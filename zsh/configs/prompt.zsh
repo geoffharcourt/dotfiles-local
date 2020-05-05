21ffa0d6554b10368b827758e382844fa21f48bd
Geoff Harcourt <geoff.harcourt@gmail.com>
2020-05-05 06:43:01 -0400

    Use rust-based Pure prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,3 +1,14 @@
 # pure
-autoload -U promptinit && promptinit
-prompt pure
+function zle-line-init zle-keymap-select {
+  PROMPT=`~/.bin/purs prompt -k "$KEYMAP" -r "$?" --venv "${${VIRTUAL_ENV:t}%-*}"`
+  zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+autoload -Uz add-zsh-hook
+
+function _prompt_purs_precmd() {
+  ~/.bin/purs precmd
+}
+add-zsh-hook precmd _prompt_purs_precmd
