083c7583f3e341c942c5f654de03b510914dbab4
Geoff Harcourt <geoff.harcourt@gmail.com>
2024-03-12 15:21:05 -0400

    Move from purs to pure prompt

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,14 +1,3 @@
-# pure
-function zle-line-init zle-keymap-select {
-  PROMPT=`~/.bin/purs prompt -k "$KEYMAP" -r "$?" --venv "${${VIRTUAL_ENV:t}%-*}"`
-  zle reset-prompt
-}
-zle -N zle-line-init
-zle -N zle-keymap-select
-
-autoload -Uz add-zsh-hook
-
-function _prompt_purs_precmd() {
-  ~/.bin/purs precmd
-}
-add-zsh-hook precmd _prompt_purs_precmd
+fpath+="/opt/homebrew/share/zsh/site-functions"
+autoload -U promptinit; promptinit
+prompt pure
